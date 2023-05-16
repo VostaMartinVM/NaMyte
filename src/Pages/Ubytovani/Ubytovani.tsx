4db0984d4349e27e039715fafd143d33fbd1bad2
Murloc-IRL <129620705+Murloc-IRL@users.noreply.github.com>
2023-05-16 20:40:08 +0200

    despair

--- src/Pages/Ubytovani/Ubytovani.tsx
+++ src/Pages/Ubytovani/Ubytovani.tsx
@@ -23,11 +23,10 @@ const Ubytovani: FC = () => {
         <h1></h1>
         <p>
           Nabízíme ubytování v pokojích po jednom, dvou a tří lůžkách <br />
-          Cena za jednu noc se snídaní včetně parkování: <i>890 ,-Kč</i> <br />
           Každý pokoj je vybaven vlastním sociálním zařízením a LCD televizorem <br />
+          V nabídce máme i možnost přistýlky <br />
           V celé budově je dostupný internet <br />
-          Soukromé parkoviště se na noc uzamyká <br />
-          Nabízíme i možnost přistýlky <br />
+          V ceně je snídaně i parkování na soukromém parkovišti, které se na noc uzamyká <br />
         </p>
       </div>
     </div>
